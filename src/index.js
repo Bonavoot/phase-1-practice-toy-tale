7e007921147a332111d36590647365a679512cd2
Joseph <Jbonventre95@gmail.com>
2022-11-29 18:32:02 -0500

    Projecto completeru

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // Selectors
 const toyCollection = document.getElementById("toy-collection");
+const submitNewToyBtn = document.getElementById("add-toy-form");
 
 fetch("http://localhost:3000/toys")
   .then((response) => {
@@ -10,23 +11,40 @@ fetch("http://localhost:3000/toys")
       const toyCard = document.createElement("div");
       toyCard.className = "card";
 
+      // name
       const name = document.createElement("h2");
-      name.innerText = toy.name;
+      name.textContent = toy.name;
 
+      // image
       const img = document.createElement("img");
       img.className = "toy-avatar";
       img.src = toy.image;
 
       const likes = document.createElement("p");
-      likes.innerText = toy.likes;
+      likes.textContent = toy.likes;
 
       const button = document.createElement("button");
       button.className = "like-btn";
       button.id = toy.id;
       button.innerText = "Like ❤️";
+      button.addEventListener("click", (e) => {
+        let addLike = parseInt(likes.textContent) + 1;
+        likes.textContent = addLike;
+
+        fetch(`http://localhost:3000/toys/${button.id}`, {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+
+          body: JSON.stringify({
+            likes: addLike,
+          }),
+        });
+      });
 
       toyCard.append(name, img, likes, button);
-
       toyCollection.append(toyCard);
     });
   });
@@ -47,12 +65,25 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-fetch("http://localhost:3000/toys", {
-  method: "POST", // *GET, POST, PUT, DELETE, etc.
+async function postNewToy() {
+  const userToyName = document.getElementById("toy-name");
+  const userToyImageUrl = document.getElementById("toy-image");
 
-  headers: {
-    "Content-Type": "application/json",
-  },
+  await fetch("http://localhost:3000/toys", {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
 
-  body: JSON.stringify(), // body data type must match "Content-Type" header
-});
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+
+    body: JSON.stringify({
+      name: userToyName.value,
+      image: userToyImageUrl.value,
+      likes: 0,
+    }), // body data type must match "Content-Type" header
+  });
+}
+
+// Add new toy function invoked
+submitNewToyBtn.addEventListener("submit", postNewToy);
