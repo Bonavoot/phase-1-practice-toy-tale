3b997a42a4fceb79341be1efb08a1ed264d76bd3
Joseph <Jbonventre95@gmail.com>
2022-11-29 16:30:18 -0500

    Created GET

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
+// Selectors
+const toyCollection = document.getElementById("toy-collection");
+
+fetch("http://localhost:3000/toys")
+  .then((response) => {
+    return response.json();
+  })
+  .then((toys) => {
+    return toys.forEach((toy) => {
+      const toyCard = document.createElement("div");
+      toyCard.className = "card";
+
+      const name = document.createElement("h2");
+      name.innerText = toy.name;
+
+      const img = document.createElement("img");
+      img.className = "toy-avatar";
+      img.src = toy.image;
+
+      const likes = document.createElement("p");
+      likes.innerText = toy.likes;
+
+      const button = document.createElement("button");
+      button.className = "like-btn";
+      button.id = toy.id;
+      button.innerText = "Like ❤️";
+
+      toyCard.append(name, img, likes, button);
+
+      toyCollection.append(toyCard);
+    });
+  });
+
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -12,4 +45,14 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+});
+
+fetch("http://localhost:3000/toys", {
+  method: "POST", // *GET, POST, PUT, DELETE, etc.
+
+  headers: {
+    "Content-Type": "application/json",
+  },
+
+  body: JSON.stringify(), // body data type must match "Content-Type" header
 });
